8f187799f6944ff713d6fc8455b58ce2343b05df
unknown <eric.eduardo99@gmail.com>
2021-09-24 10:27:20 -0300

    Teste pasta oi mundo

---
+++ Gerenciador/src/main/java/br/com/alura/gerenciador/servlet/OiMundo.java
@@ -0,0 +1,18 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class OiMundo extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(arg0, arg1);
+	}
+
+}
